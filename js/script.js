988593a0fef6759874ec526e6aeecb894e2b91f1
Erildo07 <erildonunes12@gmail.com>
2025-04-25 00:08:00 -0400

    aprimodando code

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ const jump = () => {
 
 document.addEventListener('keydown', jump);
 document.addEventListener('touchstart', jump);
+
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+if (isMobile) {
+  console.log('Usuário está no celular');
+  // Aqui você pode ativar controles por toque, esconder botões, mudar UI...
+} else {
+  console.log('Usuário está no PC');
+}
